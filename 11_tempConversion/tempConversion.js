437a1c8afeff918d1ec8fac1dcc172c5d0f0abea
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-07 17:36:35 +0700

    Complete Js exercise 10

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitDeg) {
+  let celsiusDeg = (farenheitDeg - 32) * 5/9;
+  return Number(celsiusDeg.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDeg) {
+  let farenheitDeg = celsiusDeg * 9/5 + 32;
+  return Number(farenheitDeg.toFixed(1));
 };
 
 // Do not edit below this line
