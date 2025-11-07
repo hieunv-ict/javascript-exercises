b43e0b65fa9c8539906ed2d06c4de11473734b37
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-07 09:32:24 +0700

    Complete Js exercise 08 using sort function in array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    let result = arr;
+    for (let target of targets){
+        result = result.filter((item) => item !== target);
+    }
+    return result;
+    
 };
 
 // Do not edit below this line
