beb53ee76476cfea1d2ce5bf096fddf83c05c848
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-10 18:02:19 +0700

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    return arr.reduce((tmp, person) =>{
+        let birth = person.yearOfBirth;
+        let death = person.yearOfDeath;
+        if (death == undefined){
+            death = (new Date()).getFullYear();
+        }
+        let age = death - birth;
+        if (age > oldest){
+            oldest = age;
+            tmp = person;
+        }
+        return tmp;
+    },{})
 
 };
 
