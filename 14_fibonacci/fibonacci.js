174a4d7cd216b2a6dc946970a2e62c22c4e9e3c9
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-10 17:42:22 +0700

    Complete exercise 14 using recursive

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(len) {
+    len = Number(len);
+    if (len == 1 || len == 2){
+        return 1;
+    }
+    else if (len == 0){
+        return 0;
+    }
+    else if (len < 0){
+        return "OOPS";
+    }
+    else{
+        return fibonacci(len-2) + fibonacci(len-1);
+    }
 };
 
 // Do not edit below this line
