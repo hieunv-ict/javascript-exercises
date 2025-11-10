b25282a1ca2874e0cb5a8b4d3bbe079ae530441c
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-10 17:37:10 +0700

    Complete exercise 13 using manual algorithm

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let tmp = str.split("");
+    let arr = tmp.filter(char => {
+        if (char === '!' || char === ',' || char === " " || char === ".") return false;
+        else return true;
+    })
+    let len = arr.length;
+    let stack = [];
+    if (len % 2 != 0){
+        arr.splice(len/2, 1);
+        len = arr.length;
+    }
+    for (let i = 0; i < len/2; i++){
+        stack.push(arr[i]);
+    }
+    for (let i = len/2; i < len; i++){
+        if (arr[i] === stack.pop()){
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    if (stack.length == 0) return true;
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
