8c8bb6af0c7815b25cbdf38897f1d46e4f82c851
Hieu Nguyen <nvhieu.ict@gmail.com>
2025-11-10 16:48:22 +0700

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	num1 = +num1;
+  num2 = +num2;
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+	return arg.reduce((sum, current) => sum + Number(current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  return arg.reduce((res, current) => res * Number(current), 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let res = 1;
+  for (let i = 0; i < exponent; i++){
+    res *= base;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0 || num == 1){
+    return 1;
+  }
+  else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
